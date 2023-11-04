0346b7fd90653a896b5a4e2e1b14b04b2cd645a6
junayet <muhammadjunayetmaruf@gmail.com>
2023-11-04 19:43:15 +0600

    Prject has started

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors())
+app.use(express.json())
+app.use(cookieParser())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fbi4wg4.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+
+    }
+});
+
+const verifyClient = async(req, res, next) => {
+    const token = req.cookies?.token;
+    console.log('client token', token);
+    if(!token){
+        return res.status(401).send({message: 'Unauthorised'})
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=> {
+        if(err){
+            return res.status(401).send({message: 'Unauthorised'})
+        }
+        req.user = decoded
+        next()
+    })
+    
+}
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+        const database = client.db("BlogBackend");
+        // const servicesCollection = database.collection("services");
+        // auth api
+        // app.post('/jwt', async (req, res) => {
+        //     const user = req.body;
+        //     console.log(user);
+        //     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        //         expiresIn: '1h'
+        //     })
+        //     console.log(token);
+        //     res
+        //     .cookie('token', token, {
+        //         httpOnly: true,
+        //         secure: false
+        //     })
+        //     .send({ message: 'success' })
+        // })
+        // app.post('/logout', async(req, res)=> {
+            
+        //     res.clearCookie('token', {maxAge: 0}).send({success: true})
+        // })
+
+
+        
+
+       
+
+
+
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+       
+    }
+}
+run().catch(console.dir);
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
